37bc672cab26e2040afe22a0c59847ac1942867e
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2025-11-25 17:32:10 -0300

    chore(app): add Main class entry point
    - Implemented the static main method to instantiate and run the InterfazConsola.

--- src/main/java/org/siglo21/Main.java
+++ src/main/java/org/siglo21/Main.java
@@ -1,9 +1,12 @@
 package org.siglo21;
 
+import org.siglo21.app.InterfazConsola;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        InterfazConsola interfaz = new InterfazConsola();
+        interfaz.ejecutar();
     }
 }
